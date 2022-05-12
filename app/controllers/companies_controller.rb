cdf69180f77f40e53797ef66df862850387001b7
pietro-franzero-kellogg <pietro.franzero@kellogg.northwestern.edu>
2022-05-12 02:31:22 +0000

    Adding solutions (2/2)

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -6,8 +6,19 @@ class CompaniesController < ApplicationController
 
   def show
     @company = Company.find_by({ "id" => params["id"] })
+    @contacts = Contact.where({ "company_id" => @company["id"] })
+  end
+
+  def new
+    @company = Company.new
   end
 
   def create
+    @company = Company.new
+    @company["name"] = params["company"]["name"]
+    @company["city"] = params["company"]["city"]
+    @company["state"] = params["company"]["state"]
+    @company.save
+    redirect_to "/companies"
   end
 end
